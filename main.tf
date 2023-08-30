489c4af3af24c3ac06444970083845cc967da746
wanjala <122491551+wanjala-dev@users.noreply.github.com>
2023-08-29 21:11:08 -0700

    Update main.tf
    add uniform bucket

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "google_storage_bucket" "vault_backend" {
   location      = var.location
 
   force_destroy = var.force_destroy
+  uniform_bucket_level_access = true # Enable uniform bucket-level access
 }
 
 
